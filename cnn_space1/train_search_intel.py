6fe38763fb0811531a8d5edec56113021d69398e
Bruno Sette <bruno@pop-os.localdomain>
2021-04-08 21:18:14 -0300

    imports

--- cnn_space1/train_search_intel.py
+++ cnn_space1/train_search_intel.py
@@ -12,6 +12,9 @@ import torch.utils
 import torch.nn.functional as F
 import torchvision.datasets as dset
 import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+from sklearn.model_selection import train_test_split
+import torch.backends.cudnn as cudnn
 
 from torch.autograd import Variable
 from model_search import Network
