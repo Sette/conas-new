dda88db6732f24a28d911f7d95221b5b824c6636
Bruno Sette <bruno@pop-os.localdomain>
2021-04-16 22:47:57 -0300

    test intel

--- cnn_space1/test_intel.py
+++ cnn_space1/test_intel.py
@@ -30,6 +30,7 @@ parser.add_argument('--cutout_length', type=int, default=16, help='cutout length
 parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
+parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 args = parser.parse_args()
 
 args.save = 'test-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
