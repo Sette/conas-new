dd48d36d98b3d7fd57d3e6070df661f6dabe64d8
Bruno Sette <bruno@pop-os.localdomain>
2021-04-18 21:35:24 -0300

    update

--- cnn_space1/train_intel.py
+++ cnn_space1/train_intel.py
@@ -125,17 +125,15 @@ def main():
       transforms.ToTensor(),
       normalize,
     ]))
-  
-   valid_data_train,valid_data = train_test_split(valid_data, test_size = 0.30,random_state=42)
+    
+  valid_data_train,val_data = train_test_split(val_data, test_size = 0.30,random_state=42)
 
 
   train_queue = torch.utils.data.DataLoader(
     train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=4)
   
- 
-  
-  
-    valid_data_train = torch.utils.data.DataLoader(
+
+  valid_data_train = torch.utils.data.DataLoader(
   valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=4)
 
   scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.decay_period, gamma=args.gamma)
