f47286905f16328692d4f0ddf475c1814aaccecc
Bruno Sette <bruno@pop-os.localdomain>
2021-04-18 21:30:59 -0300

    update

--- cnn_space1/train_intel.py
+++ cnn_space1/train_intel.py
@@ -128,7 +128,7 @@ def main():
   
   train_queue = torch.utils.data.DataLoader(
     train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=4)
-  valid_data_train valid_data = train_test_split(valid_data, test_size = 0.30,random_state=42)
+  valid_data_train,valid_data = train_test_split(valid_data, test_size = 0.30,random_state=42)
     valid_queue = torch.utils.data.DataLoader(
   valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=4)
 
