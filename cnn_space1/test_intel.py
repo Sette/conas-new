92da8126b912ef12eee2f0673db13ef8a3e7f6e9
Bruno Sette <bruno@pop-os.localdomain>
2021-04-19 17:52:46 -0300

    update

--- cnn_space1/test_intel.py
+++ cnn_space1/test_intel.py
@@ -33,6 +33,7 @@ parser.add_argument('--auxiliary', action='store_true', default=False, help='use
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
 parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
+parser.add_argument('--label_smooth', type=float, default=0.1, help='label smoothing')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
 parser.add_argument('--save', type=str, default='EXP', help='experiment name')
