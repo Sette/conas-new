4850cb1b819615e48f5f97502e5be3831af6b520
Bruno Sette <bruno@pop-os.localdomain>
2021-04-22 14:42:24 -0300

    update

--- test-ensemble_intel.py
+++ test-ensemble_intel.py
@@ -48,6 +48,21 @@ from torch.autograd import Variable
 import torchvision
 import torchvision.transforms as transforms
 
+class CrossEntropyLabelSmooth(nn.Module):
+
+  def __init__(self, num_classes, epsilon):
+    super(CrossEntropyLabelSmooth, self).__init__()
+    self.num_classes = num_classes
+    self.epsilon = epsilon
+    self.logsoftmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, inputs, targets):
+    log_probs = self.logsoftmax(inputs)
+    targets = torch.zeros_like(log_probs).scatter_(1, targets.unsqueeze(1), 1)
+    targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
+    loss = (-targets * log_probs).mean(0).sum()
+    return loss
+
 
 save = path_logits +'/test-{}-{}'.format("ENS", time.strftime("%Y%m%d-%H%M%S"))
 utils.create_exp_dir(save, scripts_to_save=glob.glob('*.py'))
