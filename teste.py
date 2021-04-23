3160b196a01d39bc82d0ce0f5c08b020e0c804a7
Bruno Sette <bruno@pop-os.localdomain>
2021-04-23 19:56:29 -0300

    teste2

--- teste.py
+++ teste.py
@@ -66,7 +66,7 @@ test_queue = torch.utils.data.DataLoader(test_data, batch_size=128, shuffle=Fals
 '''
 logits1 = pickle.load( open( path_logits1 + "/logits0.p", "rb" ) )
 print(len(logits1))
-for i in range(logits1)
+for i in range(logits1):
 	logits[i] = np.argmax(logits[i])
 
 print(logits)
